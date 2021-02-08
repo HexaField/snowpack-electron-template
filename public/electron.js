13fb3555b5b633d3a7a945df2dceddad9b7a1d8e
HexaField <joshfield999@gmail.com>
2021-02-08 23:52:22 +1100

    initial commit

---
+++ public/electron.js
@@ -0,0 +1,61 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  if (app.isPackaged) {
+    win.loadURL(`file://${path.join(__dirname, '../dist_web/index.html')}`);
+  } else if (process.env.NODE_ENV === 'production') {
+    win.loadFile('../dist_web/index.html');
+  } else {
+    win.loadURL('http://localhost:8080');
+  }
+
+  // Open the DevTools.
+  win.webContents.openDevTools();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them
+
+function electronIsPackaged() {
+  let isPackaged = false;
+
+  if (process.main && process.main.filename.indexOf('app.asar') !== -1) {
+    isPackaged = true;
+  } else if (process.argv.filter(a => a.indexOf('app.asar') !== -1).length > 0) {
+    isPackaged = true;
+  }
+
+  return isPackaged;
+}
---
+++ readme.md
@@ -0,0 +1,35 @@
+# The Ultimate 3d React Web & Electron App Template
+
+Create, test and distribute decentralised 3D apps for the browser and the desktop with ease.
+
+## Includes
+
+- Snowpack
+- Electron
+- Typescript
+- React
+- Webpack
+- Threejs
+- Jest
+- Puppeteer
+- Comlink
+
+## Completed
+
+- Snowpack, React, Typescript & Webpack
+- Electron development workflow
+- Electron distribution build
+
+## To Do
+
+- Jest & Puppeteer testing
+- Threejs canvas demo setup
+- Local server / client threaded architecture example with Comlink
+- Electron server background process
+
+
+
+notes
+https://github.com/snowpackjs/snowpack/tree/main/create-snowpack-app/cli
+https://www.snowpack.dev/guides/jest/
+https://github.com/stepanvanzuriak/snowpack-electron-react
